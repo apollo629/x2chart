34f0ff09c18da79cc710e18a4db53c9843055059
Alperen İNAL <inal.alperen@gmail.com>
2015-06-26 17:28:34 +0300

    PieChart support is added

--- src/main/java/com/apollo/x2chart/ChartFromTxt.java
+++ src/main/java/com/apollo/x2chart/ChartFromTxt.java
@@ -9,17 +9,19 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -38,6 +40,18 @@ public class ChartFromTxt {
     @Option(name="-d",usage="delimeter", metaVar="INPUT")
     private static String delimeter = ",";
 
+    @Option(name="-t",usage="title", metaVar="INPUT")
+    private static String title = "title";
+
+    @Option(name="-x",usage="xAxis", metaVar="INPUT")
+    private static String xAxis = "xAxis";
+
+    @Option(name="-y",usage="yAxis", metaVar="INPUT")
+    private static String yAxis = "yAxis";
+
+    @Option(name="-c",usage="Chart Type bar=1 pie=2", metaVar="INPUT")
+    private static int chartType = 0;
+
     public static void main(String[] args) {
         new ChartFromTxt().doMain(args);
     }
@@ -48,37 +62,43 @@ public class ChartFromTxt {
         try {
             // parse the arguments.
             parser.parseArgument(args);
-            // you can parse additional arguments if you want.
-            // parser.parseArgument("more","args");
-            // after parsing arguments, you should check
-            // if enough arguments are given.
+            if( args.length == 0 ) {
+                throw new CmdLineException(parser, "No argument is given", new Throwable("No argument is given"));
+            }
             Path path = Paths.get(input.getAbsolutePath());
             List<String> headers = readHeader(path);
-            headers.forEach(System.out::println);
 
             Stream<String> lines = Files.lines(path);
             List<String> numbers = (lines.skip(1).map(line -> line.split(",")).flatMap(Arrays::stream).collect(toList()));
 
-            System.out.println(numbers.toString());
+            if (chartType == 0)
+                throw new CmdLineException(parser, "Please choose a chart type", new Throwable("No chart type is choosen"));
+            if (chartType == 1)
+                writeChartToPDF(createBarChart(numbers, headers), 500, 400, out);
+            if (chartType == 2)
+                writeChartToPDF(createPieChart(numbers, headers), 500, 400, out);
 
-            writeChartToPDF(createBarChart(numbers,headers),500,400,out);
+            System.out.println("Chart pdf is created successfully in path: " + new File(out).getAbsolutePath());
 
-
-            if( args.length == 0 )
-                throw new CmdLineException(parser,"No argument is given");
+            if (Desktop.isDesktopSupported()) {
+                try {
+                    File myFile = new File(out);
+                    Desktop.getDesktop().open(myFile);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    // no application registered for PDFs
+                }
+            }
 
         } catch( CmdLineException e ) {
             // if there's a problem in the command line,
             // you'll get this exception. this will report
             // an error message.
             System.err.println(e.getMessage());
-            System.err.println("java SampleMain [options...] arguments...");
             // print the list of available options
             parser.printUsage(System.err);
             System.err.println();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -100,10 +120,20 @@ public class ChartFromTxt {
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
         int index = 0;
         for(String s : dataList) {
-            dataSet.setValue(Integer.valueOf(s), "Population", headers.get(index));
+            dataSet.setValue(Integer.valueOf(s), "rowKey", headers.get(index));
             index++;
         }
-        return ChartFactory.createBarChart("title","xAxis","yAxis", dataSet, PlotOrientation.VERTICAL, false, true, false);
+        return ChartFactory.createBarChart(title,xAxis,yAxis, dataSet, PlotOrientation.VERTICAL, false, true, false);
+    }
+
+    static JFreeChart createPieChart(List<String> dataList, List<String> headers){
+        DefaultPieDataset dataSet = new DefaultPieDataset();
+        int index = 0;
+        for(String s : dataList) {
+            dataSet.setValue(headers.get(index), Integer.valueOf(s));
+            index++;
+        }
+        return ChartFactory.createPieChart(title, dataSet, true, true, false);
     }
 
     static void writeChartToPDF(JFreeChart chart, int width, int height, String fileName){
